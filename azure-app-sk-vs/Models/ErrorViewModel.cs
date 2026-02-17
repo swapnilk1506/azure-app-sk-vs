5f3cc1e4a858db04e12345e202df4b6ba3e51ff8
pc <pc@SWAPNIL-DESKTOP>
2026-02-17 16:12:43 +0530

    Add project files.

---
+++ azure-app-sk-vs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace azure_app_sk_vs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
